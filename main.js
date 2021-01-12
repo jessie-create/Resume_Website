8621df1d32cc0517a66c0bfce8fe760b8bcb48a5
Jie(Jessie)Wang <68354315+jessie-create@users.noreply.github.com>
2021-01-12 18:24:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// mobile nav
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+const navName = document.querySelector('#navbar-name');
+const body = document.querySelector('body');
+
+// dispaly mobile menu
+const mobileMenu =() => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    body.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
+
+//hero typed 
